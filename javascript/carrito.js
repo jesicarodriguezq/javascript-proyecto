2e23a8be64579c77676c25f034c0f8cb0316a290
jesica rodriguez <jesicarodriguezq@gmail.com>
2023-04-06 12:38:48 -0300

    se modifico js y se agrego js carrito

---
+++ javascript/carrito.js
@@ -0,0 +1,101 @@
+const colorTema = document.querySelector('#colorTema');
+let temaOscuroLocalStorage = localStorage.getItem('temaOscuro');
+const contenedorTienda = document.querySelector('.contenedorTienda');
+const contenedorCarrito = document.querySelector('.contenedor-carrito')
+const carritoDeTienda = JSON.parse(localStorage.getItem('carrito'));
+const botonVaciarCarrito = document.querySelector('.vaciar-carrito');
+const totalCarrito = document.querySelector('.total-carrito');
+const contenedorFinalizarCompra =document.querySelector('.contenedor-finalizar-compra');
+const botonFinalizarCompra = document.querySelector('.btn-finalizar-compra');
+
+const activarTema = () => {
+    contenedorTienda.classList.add('temaOscuro');
+    localStorage.setItem('temaOscuro', 'true');
+};
+const desactivarTema = () => {
+    contenedorTienda.classList.remove('temaOscuro');
+    localStorage.setItem('temaOscuro', 'false');
+};
+if(temaOscuroLocalStorage === 'true'){
+    activarTema()
+}else {
+    desactivarTema()
+};
+colorTema.addEventListener('click', () => {
+    temaOscuroLocalStorage = localStorage.getItem('temaOscuro');
+    if(temaOscuroLocalStorage === 'true'){
+        desactivarTema()
+    }else {
+        activarTema()
+    }
+});
+function cargarProductosCarrito() {
+    if (carritoDeTienda && carritoDeTienda.length > 0) {
+        contenedorCarrito.innerHTML = '';
+        carritoDeTienda.forEach(producto => {
+            const cardCarrito = document.createElement('div');
+            cardCarrito.classList.add('card-carrito');
+            cardCarrito.innerHTML = 
+            `
+            <img class="img-producto-carrito" src="${producto.img}" alt="${producto.tipo} de ${producto.marca}">
+            <div class="texto-producto-carrito">
+                <div class="tipo-marca-producto-carrito">
+                    <h4 class="tipo-producto-carrito">${producto.tipo}</h4>
+                    <h5 class="marca-carrito">${producto.marca}</h5>
+                </div>
+                <div class="precio-eliminar-carrito">
+                    <div class="texto-precio-carrito">
+                        <p class="precio-carrito">$${producto.precio}</p>
+                    </div>
+                    <button class="eliminar-carrito" id="${producto.id}">ELIMINAR</button>
+                </div>
+            </div>
+            `
+            contenedorCarrito.append(cardCarrito)
+        });
+    btnEliminar();
+    totalDeProductosCarrito()
+    }else {
+        contenedorCarrito.innerHTML = '';
+        const sinProductos = document.createElement('h5');
+        sinProductos.classList.add('sin-producto-carrito')
+        sinProductos.innerHTML = '"No hay productos en el carrito"';
+        contenedorCarrito.append(sinProductos);
+        contenedorFinalizarCompra.classList.add('borrar-contenedor');
+    };
+};
+cargarProductosCarrito();
+function btnEliminar() {
+    const btnParaEliminar = document.querySelectorAll('.eliminar-carrito')
+    btnParaEliminar.forEach(btn => {
+        btn.addEventListener('click', eliminarCardCarrito)
+    });
+};
+function eliminarCardCarrito(e) {
+    const btnEliminarId = e.currentTarget.id;
+    const encontrarProducto = carritoDeTienda.findIndex(producto => producto.id === btnEliminarId);
+    carritoDeTienda.splice(encontrarProducto, 1);
+    cargarProductosCarrito();
+    localStorage.setItem('carrito', JSON.stringify(carritoDeTienda))
+};
+function totalDeProductosCarrito() {
+    const calcularTotal = carritoDeTienda.reduce((sum, producto) => sum + producto.precio, 0);
+    totalCarrito.innerText = `TOTAL: $${calcularTotal}`
+}
+botonVaciarCarrito.addEventListener('click', vaciarCarrito)
+function vaciarCarrito() {
+    carritoDeTienda.length = 0;
+    localStorage.setItem('carrito', JSON.stringify(carritoDeTienda));
+    cargarProductosCarrito()
+};
+botonFinalizarCompra.addEventListener('click', finalizarCompra)
+function finalizarCompra() {
+    carritoDeTienda.length = 0;
+    localStorage.setItem('carrito', JSON.stringify(carritoDeTienda));
+    contenedorFinalizarCompra.classList.add('borrar-contenedor');
+    contenedorCarrito.innerHTML = '';
+    const compraRealizada = document.createElement('h5');
+    compraRealizada.classList.add('texto-compra-realizada')
+    compraRealizada.innerHTML = '"Compra realizada"';
+    contenedorCarrito.append(compraRealizada);
+};
--- javascript/index.js
+++ javascript/index.js
@@ -1,84 +1,456 @@
 const productos = [
-{
-        id: 01,
-        marca: "GARNIER",
+        {// AGUA MICELAR
+        id: 'agua-micelar-1',
+        img: './assets/img/agua-micelar-1.jpg',
+        tipo: 'Agua micelar',
+        marca: 'GARNIER',
         precio: 2300,
-        tipo: "Agua micelar"
+        categoria: {
+                id: 'agua-micelar'
+        }
         },
         {
-        id: 02,
-        marca: "CERAVE",
-        precio: 6000,
-        tipo: "Limpiador facial"
+        id: 'agua-micelar-2',
+        img: './assets/img/agua-micelar-2.jpg',
+        tipo: 'Agua micelar',
+        marca: 'LA ROCHE-POSEY',
+        precio: 2300,
+        categoria: {
+                id: 'agua-micelar'
+        }
         },
         {
-        id: 03,
-        marca: "LOREAL",
-        precio: 4300,
-        tipo: "Serum"
+        id: 'agua-micelar-3',
+        img: './assets/img/agua-micelar-3.jpg',
+        tipo: 'Agua micelar',
+        marca: 'CERAVE',
+        precio: 2300,
+        categoria: {
+                id: 'agua-micelar'
+        }
         },
         {
-        id: 04,
-        marca: "LOREAL PARIS",
-        precio: 5900,
-        tipo: "Contorno de ojos"
+        id: 'agua-micelar-4',
+        img: './assets/img/agua-micelar-4.jpg',
+        tipo: 'Agua micelar',
+        marca: 'NEUTROGENA',
+        precio: 2800,
+        categoria: {
+                id: 'agua-micelar'
+        }
         },
         {
-        id: 05,
-        marca: "CETAPHIL",
-        precio: 5600,
-        tipo: "Crema hidratante"
+        id: 'agua-micelar-5',
+        img: './assets/img/agua-micelar-5.jpg',
+        tipo: 'Agua micelar',
+        marca: 'BIODERMA',
+        precio: 2800,
+        categoria: {
+                id: 'agua-micelar'
+        }
         },
         {
-        id: 06,
-        marca: "DERMAGLOS",
+        id: 'agua-micelar-6',
+        img: './assets/img/agua-micelar-6.jpg',
+        tipo: 'Agua micelar',
+        marca: 'LOREAL',
+        precio: 2800,
+        categoria: {
+                id: 'agua-micelar'
+        }
+        },
+        //LIMPIADOR FACIAL
+        { 
+        id: 'limpiador-facial-1',
+        img: './assets/img/limpiador-facial-1.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'GARNIER',
+        precio: 2480,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        {
+        id: 'limpiador-facial-2',
+        img: './assets/img/limpiador-facial-2.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'LA ROCHE-POSEY',
+        precio: 2480,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        {
+        id: 'limpiador-facial-3',
+        img: './assets/img/limpiador-facial-3.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'CERAVE',
+        precio: 2480,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        {
+        id: 'limpiador-facial-4',
+        img: './assets/img/limpiador-facial-4.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'NEUTROGENA',
+        precio: 3500,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        {
+        id: 'limpiador-facial-5',
+        img: './assets/img/limpiador-facial-5.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'BIODERMA',
+        precio: 3500,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        {
+        id: 'limpiador-facial-6',
+        img: './assets/img/limpiador-facial-6.jpg',
+        tipo: 'Limpiador facial',
+        marca: 'LOREAL',
+        precio: 3500,
+        categoria: {
+                id: 'limpiador-facial'
+        }
+        },
+        //SERUM
+        { 
+        id: 'serum-1',
+        img: './assets/img/serum-1.jpg',
+        tipo: 'Serum',
+        marca: 'GARNIER',
+        precio: 5690,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        {
+        id: 'serum-2',
+        img: './assets/img/serum-2.jpg',
+        tipo: 'Serum',
+        marca: 'LA ROCHE-POSEY',
+        precio: 5690,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        {
+        id: 'serum-3',
+        img: './assets/img/serum-3.jpg',
+        tipo: 'Serum',
+        marca: 'CERAVE',
+        precio: 5690,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        {
+        id: 'serum-4',
+        img: './assets/img/serum-4.jpg',
+        tipo: 'Serum',
+        marca: 'NEUTROGENA',
+        precio: 9790,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        {
+        id: 'serum-5',
+        img: './assets/img/serum-5.jpg',
+        tipo: 'Serum',
+        marca: 'BIODERMA',
+        precio: 9790,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        {
+        id: 'serum-6',
+        img: './assets/img/serum-6.jpg',
+        tipo: 'Serum',
+        marca: 'LOREAL',
+        precio: 9790,
+        categoria: {
+                id: 'serum'
+        }
+        },
+        //CONTORNO DE OJOS
+        { 
+        id: 'contorno-de-ojos-1',
+        img: './assets/img/contorno-ojos-1.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'GARNIER',
+        precio: 2400,
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        {
+        id: 'contorno-de-ojos-2',
+        img: './assets/img/contorno-ojos-2.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'LA ROCHE-POSEY',
+        precio: 2400,
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        {
+        id: 'contorno-de-ojos-3',
+        img: './assets/img/contorno-ojos-3.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'CERAVE',
+        precio: 2400,
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        {
+        id: 'contorno-de-ojos-4',
+        img: './assets/img/contorno-ojos-4.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'NEUTROGENA',
         precio: 2900,
-        tipo: "Protector solar"
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        {
+        id: 'contorno-de-ojos-5',
+        img: './assets/img/contorno-ojos-5.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'BIODERMA',
+        precio: 2900,
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        {
+        id: 'contorno-de-ojos-6',
+        img: './assets/img/contorno-ojos-6.jpg',
+        tipo: 'Contorno de ojos',
+        marca: 'LOREAL',
+        precio: 2900,
+        categoria: {
+                id: 'contorno-ojos'
+        }
+        },
+        //CREMA HIDRATANTE
+        { 
+        id: 'crema-hidratante-1',
+        img: './assets/img/crema-hidratante-1.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'GARNIER',
+        precio: 3400,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        {
+        id: 'crema-hidratante-2',
+        img: './assets/img/crema-hidratante-2.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'LA ROCHE-POSEY',
+        precio: 3400,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        {
+        id: 'crema-hidratante-3',
+        img: './assets/img/crema-hidratante-3.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'CERAVE',
+        precio: 3400,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        {
+        id: 'crema-hidratante-4',
+        img: './assets/img/crema-hidratante-4.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'NEUTROGENA',
+        precio: 6100,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        {
+        id: 'crema-hidratante-5',
+        img: './assets/img/crema-hidratante-5.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'BIODERMA',
+        precio: 6100,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        {
+        id: 'crema-hidratante-6',
+        img: './assets/img/crema-hidratante-6.jpg',
+        tipo: 'Crema hidratante',
+        marca: 'LOREAL',
+        precio: 6100,
+        categoria: {
+                id: 'crema-hidratante'
+        }
+        },
+        //PROTECTOR SOLAR
+        { 
+        id: 'protector-solar-1',
+        img: './assets/img/protector-solar-1.jpg',
+        tipo: 'Protector solar',
+        marca: 'GARNIER',
+        precio: 5800,
+        categoria: {
+                id: 'protector-solar'
+        }
+        },
+        {
+        id: 'protector-solar-2',
+        img: './assets/img/protector-solar-2.jpg',
+        tipo: 'Protector solar',
+        marca: 'LA ROCHE-POSEY',
+        precio: 5800,
+        categoria: {
+                id: 'protector-solar'
+        }
+        },
+        {
+        id: 'protector-solar-3',
+        img: './assets/img/protector-solar-3.jpg',
+        tipo: 'Protector solar',
+        marca: 'CERAVE',
+        precio: 5800,
+        categoria: {
+                id: 'protector-solar'
+        }
+        },
+        {
+        id: 'protector-solar-4',
+        img: './assets/img/protector-solar-4.jpg',
+        tipo: 'Protector solar',
+        marca: 'NEUTROGENA',
+        precio: 7300,
+        categoria: {
+                id: 'protector-solar'
+        }
+        },
+        {
+        id: 'protector-solar-5',
+        img: './assets/img/protector-solar-5.jpg',
+        tipo: 'Protector solar',
+        marca: 'BIODERMA',
+        precio: 7300,
+        categoria: {
+                id: 'protector-solar'
+        }
+        },
+        {
+        id: 'protector-solar-6',
+        img: './assets/img/protector-solar-6.jpg',
+        tipo: 'Protector solar',
+        marca: 'LOREAL',
+        precio: 7300,
+        categoria: {
+                id: 'protector-solar'
+        }
         }
 ];
-        
-let carritoDeCompras = [];    
-let todosProductos = productos.map(producto => `${producto.id} - ${producto.tipo} de ${producto.marca} a $${producto.precio}`);
-        
-const bienvenida = alert(`¡Bienvenido/a a SkincareShop!`);
 
-function agregarAlCarrito () {
-let productoEncontrado = ""
-let agregarProducto = Number(prompt(`Seleccione un producto para agregar al carrito con número: \n${todosProductos.join("\n")}`));
-        if (agregarProducto > 0 && agregarProducto < productos.length) {
-        productoEncontrado = productos.find(producto => producto.id === agregarProducto)
-        carritoDeCompras.push(productoEncontrado);
-        } else {
-        alert(`No se encontró el producto: ${agregarProducto}`);
-        }
+    //------------------------------------------------------------------------------------------------
+
+const btnArrow = document.querySelector('.btnDeslizar');
+const colorTema = document.querySelector('#colorTema');
+let temaOscuroLocalStorage = localStorage.getItem('temaOscuro');
+const contenedorTienda = document.querySelector('.contenedorTienda');
+const contenedorProductos = document.querySelector('#contenedorProductos');
+const btnCategorias = document.querySelectorAll('.btn-categoria');
+
+const btnScrollClick = btnArrow.addEventListener('click', () => {
+        window.scroll({
+        top: innerHeight,
+        left: 0,
+        behavior: "smooth",
+        });
+});
+const activarTema = () => {
+        contenedorTienda.classList.add('temaOscuro');
+        localStorage.setItem('temaOscuro', 'true');
 };
-
-agregarAlCarrito();
-
-
-let agregarMas = prompt(`¿Quieres comprar otro producto? Responder con si o no`)
-        
-while(agregarMas === `si`){
-        agregarAlCarrito()
-        agregarMas = prompt(`¿Te gustaría otro producto? Responder con si o no`)
-}
-
-console.log(carritoDeCompras);
-
-let buscarProductos = carritoDeCompras.map(producto => `${producto.tipo} de ${producto.marca} a $${producto.precio}`);
-let productosEnCarrito = alert(`CARRITO: \n${buscarProductos.join("\n")}`);
-
-
-function totalPagar () {
-        const total = carritoDeCompras.reduce((cosas, cadaProducto) => cosas + cadaProducto.precio, 0)
-        let preguntaDePagar = prompt(`¿Desea pagar el total? (si o no)
-        Total: $${total}`)
-        if (preguntaDePagar === `si`) {
-        alert(`Debe pagar un total de $${total}`)
-        }else{
-        alert(`Salga de la tienda`)
+const desactivarTema = () => {
+        contenedorTienda.classList.remove('temaOscuro');
+        localStorage.setItem('temaOscuro', 'false');
+};
+if(temaOscuroLocalStorage === 'true'){
+        activarTema()
+}else {
+        desactivarTema()
+};
+colorTema.addEventListener('click', () => {
+        temaOscuroLocalStorage = localStorage.getItem('temaOscuro');
+        if(temaOscuroLocalStorage === 'true'){
+        desactivarTema()
+        }else {
+        activarTema()
         }
+});
+function cargarProductos(elegirProductoPorCategoria) {
+        contenedorProductos.innerHTML = ' ';
+        elegirProductoPorCategoria.forEach(producto => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = 
+        `
+                <img class="img-producto" src="${producto.img}" alt="${producto.tipo} de ${producto.marca}">
+                <div class="texto-producto">
+                <h4 class="tipo-producto">${producto.tipo}</h4>
+                <h5 class="marca">${producto.marca}</h5>
+                <p class="precio">$${producto.precio}</p>
+                <button class="btn-agregar-carrito" id="${producto.id}">Agregar al carrito</button>
+                </div>
+        `;
+        contenedorProductos.append(card);
+        });
+};
+cargarProductos(productos);
+btnCategorias.forEach( cadaCategoria => {
+        cadaCategoria.addEventListener('click', (e) => {
+        btnCategorias.forEach( cadaCategoria => cadaCategoria.classList.remove('seleccionoCategoria'));
+        e.currentTarget.classList.add('seleccionoCategoria');
+        if(e.currentTarget.id != 'todos-los-productos'){
+                const productosPorCategoria = productos.filter(producto =>
+                producto.categoria.id === e.currentTarget.id);
+        cargarProductos(productosPorCategoria)
+        }else {
+                cargarProductos(productos)
+        }
+        });
+});
+
+let carrito;
+let carritoGuardado = JSON.parse(localStorage.getItem('carrito'))
+if (carritoGuardado) {
+        carrito = carritoGuardado;
+} else {
+carrito = [];
 }
-
-totalPagar();
-
+document.addEventListener('click', (e) => {
+        if (e.target.matches('.card .texto-producto .btn-agregar-carrito')) {
+        agregarAlCarrito(e)
+        }
+});
+const agregarAlCarrito = (e) =>{
+        const botonCard = e.target;
+        const buscarCard = productos.find(producto => producto.id ===  botonCard.id);
+        carrito.push(buscarCard);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+}
